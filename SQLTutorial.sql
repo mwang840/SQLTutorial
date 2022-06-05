fbed244e04d64b4478b5aaba0ca88e0db4bc4d72
Maxwell Wang <maxwang@udel.edu>
2022-06-04 21:55:36 -0400

    made a demo with sql accessing databases

--- SQLTutorial.sql
+++ SQLTutorial.sql
@@ -1,11 +1,5 @@
 -- Create a new database called 'TutorialDB'
 -- Connect to the 'master' database to run this snippet
-USE master
-GO
-IF NOT EXISTS (
-   SELECT name
-   FROM sys.databases
-   WHERE name = N'TutorialDB'
-)
-CREATE DATABASE [TutorialDB]
-GO
+create database edureka;
+
+drop database edureka;
